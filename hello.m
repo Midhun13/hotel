9ad0e6a49f1ff527b6cfc01736e5bc7051747304
Midhun M.B <110243906+Midhun13@users.noreply.github.com>
2025-04-09 11:58:23 +0530

    Add files via upload

---
+++ hello.m
@@ -0,0 +1,7 @@
+% This is a comment - anything after the '%' symbol is ignored by MATLAB
+
+% Define a function or script name
+function myScript
+    % Your code here
+    disp('Hello, World!');
+end
